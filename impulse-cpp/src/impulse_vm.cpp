e9f745f2c15a7c91c7f89cddec8886090f0b7614
Jesse Granden <jesse.granden@gmail.com>
2026-08-14 16:53:41 -0500

    perf(cpp): add hardware cacheline prefetching to CSR and 2-hop graph walks

--- impulse-cpp/src/impulse_vm.cpp
+++ impulse-cpp/src/impulse_vm.cpp
@@ -1252,11 +1252,21 @@ op_CSR_WALK_2HOP: {
                         uint32_t start1 = slot1.offsets_ptr[u];
                         uint32_t end1 = slot1.offsets_ptr[u + 1];
                         for (uint32_t i = start1; i < end1; ++i) {
+                            if (i + 4 < end1) {
+                                uint32_t next_v = slot1.targets_ptr[i + 4];
+                                if (next_v < slot2.node_count) {
+                                    __builtin_prefetch(&slot2.offsets_ptr[next_v], 0, 1);
+                                }
+                            }
                             uint32_t v = slot1.targets_ptr[i];
                             if (v < slot2.node_count) {
                                 uint32_t start2 = slot2.offsets_ptr[v];
                                 uint32_t end2 = slot2.offsets_ptr[v + 1];
                                 for (uint32_t j = start2; j < end2; ++j) {
+                                    if (j + 8 < end2) {
+                                        __builtin_prefetch(&slot2.targets_ptr[j + 8], 0, 1);
+                                        __builtin_prefetch(&bs_dst.words[slot2.targets_ptr[j + 4] >> 6], 1, 3);
+                                    }
                                     bitset_add(bs_dst, slot2.targets_ptr[j], vm_state->query_context->max_nodes);
                                 }
                             }
@@ -1270,11 +1280,21 @@ op_CSR_WALK_2HOP: {
                 uint32_t start1 = slot1.offsets_ptr[u];
                 uint32_t end1 = slot1.offsets_ptr[u + 1];
                 for (uint32_t i = start1; i < end1; ++i) {
+                    if (i + 4 < end1) {
+                        uint32_t next_v = slot1.targets_ptr[i + 4];
+                        if (next_v < slot2.node_count) {
+                            __builtin_prefetch(&slot2.offsets_ptr[next_v], 0, 1);
+                        }
+                    }
                     uint32_t v = slot1.targets_ptr[i];
                     if (v < slot2.node_count) {
                         uint32_t start2 = slot2.offsets_ptr[v];
                         uint32_t end2 = slot2.offsets_ptr[v + 1];
                         for (uint32_t j = start2; j < end2; ++j) {
+                            if (j + 8 < end2) {
+                                __builtin_prefetch(&slot2.targets_ptr[j + 8], 0, 1);
+                                __builtin_prefetch(&bs_dst.words[slot2.targets_ptr[j + 4] >> 6], 1, 3);
+                            }
                             bitset_add(bs_dst, slot2.targets_ptr[j], vm_state->query_context->max_nodes);
                         }
                     }
@@ -1424,6 +1444,10 @@ op_CSR_WALK: {
                             uint32_t start = slot.offsets_ptr[u];
                             uint32_t end = slot.offsets_ptr[u + 1];
                             for (uint32_t i = start; i < end; ++i) {
+                                if (i + 8 < end) {
+                                    __builtin_prefetch(&slot.targets_ptr[i + 8], 0, 1);
+                                    __builtin_prefetch(&bs_dst.words[slot.targets_ptr[i + 4] >> 6], 1, 3);
+                                }
                                 bitset_add(bs_dst, slot.targets_ptr[i], vm_state->query_context->max_nodes);
                             }
                         }
@@ -1436,6 +1460,10 @@ op_CSR_WALK: {
                 uint32_t start = slot.offsets_ptr[u];
                 uint32_t end = slot.offsets_ptr[u + 1];
                 for (uint32_t i = start; i < end; ++i) {
+                    if (i + 8 < end) {
+                        __builtin_prefetch(&slot.targets_ptr[i + 8], 0, 1);
+                        __builtin_prefetch(&bs_dst.words[slot.targets_ptr[i + 4] >> 6], 1, 3);
+                    }
                     bitset_add(bs_dst, slot.targets_ptr[i], vm_state->query_context->max_nodes);
                 }
             }
